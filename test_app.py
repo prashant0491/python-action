428d7fa6f294e9b52fc7edbcfe6ace2af71531fa
Ubuntu <ubuntu@ip-172-31-40-163.ap-south-1.compute.internal>
2025-03-09 06:27:27 +0000

    Python Ci/Cd Using GitHub Actions..git add .

--- test_app.py
+++ test_app.py
@@ -9,4 +9,4 @@ def client():
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b"Hello, CI/CD with GitHub Actions!"
+    assert response.data == b"Hello, Prasad...!!!"
